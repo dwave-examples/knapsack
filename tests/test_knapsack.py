3c333bbd0a9da09f20c3b906556b54deb4df62d3
Hemant Pawar <hpawar@dwavesys.com>
2020-09-01 22:44:47 +0000

    Incorporated review comments and updated to retry few times if energy does not match

--- tests/test_knapsack.py
+++ tests/test_knapsack.py
@@ -63,25 +63,26 @@ class TestSmoke(unittest.TestCase):
         """ Verify contents of output """
         
         file_path = os.path.join(example_dir, 'main.py')
-        output = subprocess.check_output([sys.executable, file_path])
 
-        output = str(output).upper()
-        if os.getenv('DEBUG_OUTPUT'):
-            print("Example output :-"+ output)
-        
-        # Get energy value returned by solution
-        index = output.index("ENERGY") + 7
-        energy = ""
-        i = 0
-        while i < 5 and len(output)> index:
-            energy = energy + output[index]
-            i = i + 1
-            index = index + 1
-        energy = (float(energy))
-        
+        # Due to heuristic nature of solver/solution if energy doesn't match the expected one retry few times
+        runcount = 0
+        energy = 0
+        while runcount < 3 and not -524 < energy <= -519:
+            output = subprocess.check_output([sys.executable, file_path])
+            output = str(output).upper()
+
+            if os.getenv('DEBUG_OUTPUT'):
+                print("Example output :-"+ output)
+            
+            # Get energy value returned by solution
+            import re
+            regex = r'ENERGY\s*([+-]?\d+(\.\d*)?)'
+            energy = float(next(re.finditer(regex, output, re.I)).group(1))
+            runcount += 1
+
         with self.subTest(msg="Verify if output contains 'FOUND SOLUTION' \n"):
             self.assertIn("FOUND SOLUTION", output)
-        with self.subTest(msg="Verify if the energy needs to be between -519 and -524' \n"):
+        with self.subTest(msg="Verify energy is between -519 and -524'\n"):
             self.assertTrue(-524 < energy <= -519)
         with self.subTest(msg="Verify if error string contains in output \n"):
             self.assertNotIn("ERROR", output)
